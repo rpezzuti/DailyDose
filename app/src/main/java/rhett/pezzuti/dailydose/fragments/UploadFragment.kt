6856debba904aa21c57ecc3e0d4c4276e6403521
Rhett Pezzuti <rpezzuti@gmail.com>
2021-01-06 22:41:14 -0800

    works in background.

--- app/src/main/java/rhett/pezzuti/dailydose/fragments/UploadFragment.kt
+++ app/src/main/java/rhett/pezzuti/dailydose/fragments/UploadFragment.kt
@@ -8,6 +8,7 @@ import android.content.DialogInterface
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,20 +17,30 @@ import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.messaging.FirebaseMessaging
 import rhett.pezzuti.dailydose.R
 import rhett.pezzuti.dailydose.databinding.FragmentUploadBinding
 import rhett.pezzuti.dailydose.utils.sendNotification
 import rhett.pezzuti.dailydose.viewmodels.UploadViewModel
+import timber.log.Timber
 
 class UploadFragment : Fragment() {
 
     private lateinit var binding: FragmentUploadBinding
     private lateinit var viewModel: UploadViewModel
 
+    private val TOPIC = "dubstep"
+
+    companion object {
+        private const val TAG = "UploadFragment"
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        Timber.i("init token")
 
         binding = DataBindingUtil.inflate(
             inflater,
@@ -39,25 +50,49 @@ class UploadFragment : Fragment() {
         )
 
         viewModel = ViewModelProvider(this).get(UploadViewModel::class.java)
-
-
         binding.uploadViewModelXML = viewModel
         binding.lifecycleOwner = this
 
+
+        createChannel(
+            getString(R.string.fcm_test_notification_channel_id),
+            getString(R.string.fcm_test_notification_channel_name)
+        )
+
+        createChannel(
+            getString(R.string.notification_channel_id),
+            getString(R.string.notification_channel_name)
+        )
+
+
+
+        subscribeTopic()
         binding.buttonUpload.setOnClickListener {
+            val builder = AlertDialog.Builder(context)
+                .setTitle("UPLOAD?!")
+                .setMessage("Are you sure you want to upload?")
 
-            createChannel(
-                getString(R.string.notification_channel_id),
-                getString(R.string.notification_channel_name)
+            // When you click outside, it will not close.
+            builder.setCancelable(false)
+
+            builder.setPositiveButton(
+                "Yes, Upload",
+                DialogInterface.OnClickListener { dialog, id ->
+                    Toast.makeText(context, "Uplaoded", Toast.LENGTH_SHORT).show()
+                }
             )
 
-            createChannel(
-                getString(R.string.fcm_test_notification_channel_id),
-                getString(R.string.fcm_test_notification_channel_name)
+            builder.setNegativeButton(
+                "No, wait",
+                DialogInterface.OnClickListener { dialog, id ->
+                    Toast.makeText(context, "Didn't upload", Toast.LENGTH_SHORT).show()
+                }
             )
+
+            builder.create()
+
+            builder.show()
         }
-
-
         return binding.root
     }
 
@@ -80,6 +115,16 @@ class UploadFragment : Fragment() {
             val notificationManager = requireActivity().getSystemService(NotificationManager::class.java)
             notificationManager.createNotificationChannel(notificationChannel)
         }
+    }
+    private fun subscribeTopic() {
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+            .addOnCompleteListener { task ->
+                var msg = getString(R.string.message_subscribed)
+                if (!task.isSuccessful) {
+                    msg = getString(R.string.message_subscribe_failed)
+                }
+                Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+            }
     }
 
 }
--- app/src/main/java/rhett/pezzuti/dailydose/network/MyFirebaseMessagingService.kt
+++ app/src/main/java/rhett/pezzuti/dailydose/network/MyFirebaseMessagingService.kt
@@ -1,12 +1,32 @@
 package rhett.pezzuti.dailydose.network
 
+import android.app.NotificationManager
 import android.util.Log
+import androidx.core.content.ContextCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import rhett.pezzuti.dailydose.utils.sendNotification
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
 
     companion object {
         private const val TAG = "MyFirebaseMsgService"
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+
+        // This chunk is triggered when the remoteMessage is received when the app is in the foreground.
+        // When the app is in the background, the device receives the notification as normal and wanted.
+
+        remoteMessage?.data?.let {
+            Log.d(TAG, "Remote message data payload: " + remoteMessage.data)
+        }
+
+
+        remoteMessage?.notification?.let {
+            Log.d(TAG, "Message notification body: ${it.body}")
+            sendNotification(it.body!!)
+        }
     }
 
     override fun onNewToken(token: String) {
@@ -17,11 +37,15 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
 
     /**
      * Persist token to third-party (your app) servers.
-     *
-     * @param token The new token.
      */
     private fun sendRegistrationToServer(token: String?) {
-        // TODO: Implement this method to send token to your app server.
+        // Send the token to your server
+    }
+
+
+    private fun sendNotification(messageBody: String) {
+        val notificationManager = ContextCompat.getSystemService(applicationContext, NotificationManager::class.java) as NotificationManager
+        notificationManager.sendNotification(messageBody, applicationContext)
     }
 
 
