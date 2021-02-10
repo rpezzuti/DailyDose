2c2f3c74ed7fe42d93f8dd9518300a75428edbec
Rhett Pezzuti <rpezzuti@gmail.com>
2021-02-09 18:03:24 -0800

    sharedPref for topic booleans, used to save checkbox isChecked state

--- app/src/main/java/rhett/pezzuti/dailydose/fragments/PreferencesFragment.kt
+++ app/src/main/java/rhett/pezzuti/dailydose/fragments/PreferencesFragment.kt
@@ -1,6 +1,7 @@
 package rhett.pezzuti.dailydose.fragments
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,9 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LiveData
 import rhett.pezzuti.dailydose.R
-import rhett.pezzuti.dailydose.database.User
 import rhett.pezzuti.dailydose.database.getInstance
 import rhett.pezzuti.dailydose.databinding.FragmentPreferencesBinding
 import rhett.pezzuti.dailydose.factory.PreferencesViewModelFactory
@@ -23,15 +22,12 @@ class PreferencesFragment : Fragment() {
     private lateinit var viewModel: PreferencesViewModel
     private lateinit var viewModelFactory: PreferencesViewModelFactory
 
-    private val TOPIC_TEST = "Test"
-
+    // Called after onCreateView() returns
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val app = requireNotNull(this.activity).application
         val preferences = getInstance(app.applicationContext).userPreferencesDao
-
-
 
     }
 
@@ -47,15 +43,14 @@ class PreferencesFragment : Fragment() {
             false
         )
 
-        val highScore: Int = 0
-        val username: String = "Rhett"
-
         val app = requireNotNull(this.activity).application
 
         viewModelFactory = PreferencesViewModelFactory(app, binding)
         viewModel = ViewModelProvider(this, viewModelFactory).get(PreferencesViewModel::class.java)
         binding.preferencesViewModelXML = viewModel
         binding.lifecycleOwner = this
+        setupBoxes()
+
 
 
 
@@ -65,6 +60,60 @@ class PreferencesFragment : Fragment() {
         return binding.root
     }
 
+    private
+    fun setupBoxes() {
+
+        val sharedPref = activity?.getSharedPreferences(getString(R.string.user_preferences_key), Context.MODE_PRIVATE)
+
+        if (sharedPref!!.getBoolean(getString(R.string.TOPIC_DUBSTEP), false)) {
+            binding.checkboxDubstep.isChecked = true
+        }
+        if (sharedPref.getBoolean(getString(R.string.TOPIC_MELODIC_DUBSTEP), false)) {
+            binding.checkboxMelodicDubstep.isChecked = true
+        }
+        if (sharedPref.getBoolean(getString(R.string.TOPIC_MELODIC_DUBSTEP), false)) {
+            binding.checkboxMelodicDubstep.isChecked = true
+        }
+        if (sharedPref.getBoolean(getString(R.string.TOPIC_LO_FI), false)) {
+            binding.checkboxLoFi.isChecked = true
+        }
+        if (sharedPref.getBoolean(getString(R.string.TOPIC_CHILLSTEP), false)) {
+            binding.checkboxChillstep.isChecked = true
+        }
+        if (sharedPref.getBoolean(getString(R.string.TOPIC_FUTURE_GARAGE), false)) {
+            binding.checkboxFutureGarage.isChecked = true
+        }
+        if (sharedPref.getBoolean(getString(R.string.TOPIC_PIANO_AMBIENT), false)) {
+            binding.checkboxPianoAmbient.isChecked = true
+        }
+        if (sharedPref.getBoolean(getString(R.string.TOPIC_EXPERIMENTAL_BASS), false)) {
+            binding.checkboxExperimentalBass.isChecked = true
+        }
+        if (sharedPref.getBoolean(getString(R.string.TOPIC_LIQUID_DNB), false)) {
+            binding.checkboxLiquidDnb.isChecked = true
+        }
+        if (sharedPref.getBoolean(getString(R.string.TOPIC_AMBIENT_BASS), false)) {
+            binding.checkboxAmbientBass.isChecked = true
+        }
+
+
+        if (sharedPref.getBoolean(getString(R.string.TOPIC_METALCORE), false)) {
+            binding.checkboxMetalcore.isChecked = true
+        }
+        if (sharedPref.getBoolean(getString(R.string.TOPIC_ACOUSTIC_BALLADS), false)) {
+            binding.checkboxAcousticBallads.isChecked = true
+        }
+        if (sharedPref.getBoolean(getString(R.string.TOPIC_INSTRUMENTAL_ROCK), false)) {
+            binding.checkboxInstrumentalRock.isChecked = true
+        }
+        if (sharedPref.getBoolean(getString(R.string.TOPIC_DEATH_METAL), false)) {
+            binding.checkboxDeathMetal.isChecked = true
+        }
+        if (sharedPref.getBoolean(getString(R.string.TOPIC_LIVE_PERFORMANCES), false)) {
+            binding.checkboxLivePerformances.isChecked = true
+        }
+
+    }
 
 
 
