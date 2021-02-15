e8959430afb573d184630c5d1ce2a4b953696331
Rhett Pezzuti <rpezzuti@gmail.com>
2021-02-15 11:58:08 -0800

    GET a JsonObject. Now need to parse

--- app/src/main/java/rhett/pezzuti/dailydose/network/RetrofitBrowse.kt
+++ app/src/main/java/rhett/pezzuti/dailydose/network/RetrofitBrowse.kt
@@ -1,11 +1,14 @@
 package rhett.pezzuti.dailydose.network
 
+import com.google.gson.JsonObject
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
@@ -31,6 +34,11 @@ private val scalarsRetrofit = Retrofit.Builder()
     .addConverterFactory(ScalarsConverterFactory.create())
     .build()
 
+private val gsonRetrofit = Retrofit.Builder()
+    .baseUrl(FIREBASE_BASE_URL)
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
 interface BrowseFirebaseApiService {
 
     /**
@@ -45,32 +53,64 @@ interface BrowseFirebaseApiService {
             Call<List<LocalTrack>>
 
 
+    @GET("tracks/dummy-genre/track.json")
+    fun getOneTrackFromFirebase():
+            Call<Track>
+
+    // Moshi, returns a List<Track> object
+    @GET("tracks/test-genre-list.json")
+    fun getTestGenreListTrack():
+            Call<List<Track>>
+
+
     //          SCALARS
     // @GET ("tracks/test-genre") returns HTML
     // @GET ("tracks.test-genre.json) returns A full JSON object of the entire Genre
     // @GET ("tracks/test-genre/It%20Sounds%20Like%20We're%20Breaking.json") returns just that track
     // If i try to return a Call<Track> instead of Call<String>, i get a converter Error
-            // - for @GET ("tracks/test-genre/It%20Sounds%20Like%20We're%20Breaking.json")
+    // - for @GET ("tracks/test-genre/It%20Sounds%20Like%20We're%20Breaking.json")
     @GET ("tracks/test-genre.json")
     fun getOneJsonString():
             Call<String>
 
-    @GET("tracks/dummy-genre/track.json")
-    fun getOneTrackFromFirebase():
-            Call<Track>
-
-    @GET("tracks/test-genre-list.json")
-    fun getTestGenreListTrack():
-            Call<List<Track>>
-
-    @GET("tracks/test-genre-list.json")
-    fun getOneGenreFromFirebaseRepo():
-            Deferred<List<Track>>
-
+    // Moshi, gives me one track that is parsed. Returns name
     @GET("tracks/test-genre/It%20Sounds%20Like%20We're%20Breaking.json")
     fun parseOneJson():
             Call<LocalTrack>
 
+
+
+
+
+
+
+
+
+
+
+
+
+    // Gson, gives me a Json object to parse.
+    @GET ("tracks/test-genre.json")
+    fun getJsonObject():
+            Call<JsonObject>
+
+
+
+
+
+
+
+
+
+
+
+    // Moshi, used in Repo to get those two tracks.
+    @GET("tracks/test-genre-list.json")
+    fun getOneGenreFromFirebaseRepo():
+            Deferred<List<Track>>
+
+    // The dream.
     @GET (".json")
     fun refreshDatabase(): Deferred<NetworkTrackContainer>
 }
@@ -85,6 +125,12 @@ object BrowseFirebaseMoshi {
 object BrowseFirebaseScalars {
     val retrofitService : BrowseFirebaseApiService by lazy {
         scalarsRetrofit.create(BrowseFirebaseApiService::class.java)
+    }
+}
+
+object BrowseFirebaseGson {
+    val retrofitService : BrowseFirebaseApiService by lazy {
+        gsonRetrofit.create(BrowseFirebaseApiService::class.java)
     }
 }
 
--- app/src/main/java/rhett/pezzuti/dailydose/viewmodels/BrowseViewModel.kt
+++ app/src/main/java/rhett/pezzuti/dailydose/viewmodels/BrowseViewModel.kt
@@ -2,9 +2,12 @@ package rhett.pezzuti.dailydose.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.*
+import com.google.gson.JsonObject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,12 +15,14 @@ import rhett.pezzuti.dailydose.database.TrackDatabaseDao
 import rhett.pezzuti.dailydose.database.domain.LocalTrack
 import rhett.pezzuti.dailydose.database.domain.Track
 import rhett.pezzuti.dailydose.database.getInstance
+import rhett.pezzuti.dailydose.network.BrowseFirebaseGson
 import rhett.pezzuti.dailydose.network.BrowseFirebaseMoshi
 import rhett.pezzuti.dailydose.network.BrowseFirebaseScalars
 import rhett.pezzuti.dailydose.network.asDomainModel
 import rhett.pezzuti.dailydose.repository.TrackRepository
 import timber.log.Timber
 import java.io.IOException
+
 
 class BrowseViewModel(
     val trackDatabase: TrackDatabaseDao,
@@ -38,36 +43,35 @@ class BrowseViewModel(
 
     init {
         // _response.value = "broken AF"
+
+        /** Returns null right now. No such track at path exists **/
         // getOneTrackFromFirebase()
+
+        /** Displays a list of tracks from the test-genre-list child **/
         // getTestGenreListTrack()
+
+        /** Displays the JSON object of the test-genre child as a String **/
         // getTestGenreJsonString()
-        parseOneJson()
 
+        /** GETS one track from test-genre, parses it into a LocalTrack, and displays the title of that track **/
+        // parseOneJson()
 
+        /** Get a Json Object to be put into parser **/
+        getJson()
+
+        /** Gets those two tracks from the test-genre-list and shows them in recycler view **/
         viewModelScope.launch {
             trackRepository.refreshTestTracks()
         }
     }
 
+    /** Observed playlist for the recycler View **/
     val testTracks = trackRepository.tracks
 
 
-    private fun refreshTrackDatabase() = viewModelScope.launch {
-        try {
-            val playlist = BrowseFirebaseMoshi.retrofitService.refreshDatabase().await()
-            _response.value = "it fucking worked"
-            _playlist.postValue(playlist.asDomainModel())
-
-        } catch (networkError: IOException) {
-            _response.value = "Failure: " + networkError.message
-        }
-    }
-
-
-
-
     private fun getOneTrackFromFirebase() {
-        BrowseFirebaseMoshi.retrofitService.getOneTrackFromFirebase().enqueue( object: Callback<Track>{
+        BrowseFirebaseMoshi.retrofitService.getOneTrackFromFirebase().enqueue(object :
+            Callback<Track> {
             override fun onResponse(call: Call<Track>, response: Response<Track>) {
                 _response.value = response.body().toString()
             }
@@ -81,12 +85,14 @@ class BrowseViewModel(
     // Moshi Works
     // Scalars returns a converter error
     private fun getTestGenreListTrack() {
-        Timber.i("FUCK")
-        BrowseFirebaseMoshi.retrofitService.getTestGenreListTrack().enqueue( object: Callback<List<Track>>{
+        Timber.i("kek")
+        BrowseFirebaseMoshi.retrofitService.getTestGenreListTrack().enqueue(object :
+            Callback<List<Track>> {
             override fun onResponse(call: Call<List<Track>>, response: Response<List<Track>>) {
                 _response.value = response.body().toString()
                 _playlist.value = response.body()
             }
+
             override fun onFailure(call: Call<List<Track>>, t: Throwable) {
                 _response.value = "Failure: " + t.message
             }
@@ -96,9 +102,9 @@ class BrowseViewModel(
     // Scalars Works
     // Moshi returns "Expected a string but was BEGIN_OBJECT at path $"
     private fun getTestGenreJsonString() {
-        BrowseFirebaseScalars.retrofitService.getOneJsonString().enqueue( object: Callback<String>{
+        BrowseFirebaseScalars.retrofitService.getOneJsonString().enqueue(object : Callback<String> {
             override fun onResponse(call: Call<String>, response: Response<String>) {
-                _response.value = response.body().toString()
+                _response.value = response.body()
             }
 
             override fun onFailure(call: Call<String>, t: Throwable) {
@@ -106,6 +112,7 @@ class BrowseViewModel(
             }
         })
     }
+
 
 
     private fun parseOneJson() {
@@ -121,6 +128,56 @@ class BrowseViewModel(
     }
 
 
+    // Gson works
+    // Now i have a Json object to play with =]
+    private fun getJson(){
+        BrowseFirebaseGson.retrofitService.getJsonObject().enqueue(object: Callback<JsonObject>{
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                _response.value = "GREAT SUCCESS: ${response.body().toString()}"
+            }
+
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                _response.value = "Failure: " + t.message
+             }
+        })
+    }
+
+
+    private fun parseJson(data: JSONObject?) {
+
+        if (data != null) {
+
+        }
+    }
+
+
+    private fun parseJson1(data: JSONObject?) {
+        if (data != null) {
+            val it = data.keys()
+            while (it.hasNext()) {
+                val key = it.next()
+                try {
+                    if (data[key] is JSONArray) {
+                        val arry = data.getJSONArray(key)
+                        val size = arry.length()
+                        for (i in 0 until size) {
+                            parseJson(arry.getJSONObject(i))
+                        }
+                    } else if (data[key] is JSONObject) {
+                        parseJson(data.getJSONObject(key))
+                    } else {
+                        println(key + ":" + data.getString(key))
+                    }
+                } catch (e: Throwable) {
+                    try {
+                        println(key + ":" + data.getString(key))
+                    } catch (ee: Exception) {
+                    }
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
 
 
 
@@ -128,7 +185,16 @@ class BrowseViewModel(
 
 
 
+    private fun refreshTrackDatabase() = viewModelScope.launch {
+        try {
+            val playlist = BrowseFirebaseMoshi.retrofitService.refreshDatabase().await()
+            _response.value = "it fucking worked"
+            _playlist.postValue(playlist.asDomainModel())
 
+        } catch (networkError: IOException) {
+            _response.value = "Failure: " + networkError.message
+        }
+    }
 
 
 
