b33197928f56fdfe2d121d63c7397774c77ef5b9
Rhett Pezzuti <rpezzuti@gmail.com>
2021-04-16 12:00:08 -0700

    organized repository methods

--- app/src/main/java/rhett/pezzuti/dailydose/data/source/DefaultTrackRepository.kt
+++ app/src/main/java/rhett/pezzuti/dailydose/data/source/DefaultTrackRepository.kt
@@ -1,24 +1,13 @@
 package rhett.pezzuti.dailydose.data.source
 
 import androidx.lifecycle.LiveData
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
-import rhett.pezzuti.dailydose.data.DatabaseTrack
 import rhett.pezzuti.dailydose.data.Track
-import rhett.pezzuti.dailydose.data.source.remote.TrackRemoteDataSource
 
 /** Concrete Implementation of loading tracks from the data sources into the offline cache. **/
 class DefaultTrackRepository(
     private val trackLocalDataSource: TrackDataSource,
     private val trackRemoteDataSource: TrackDataSource,
-    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : TrackRepository {
-
 
     /**
      * Main access method to start request from Firebase Database. Calls updateTracksFromRemoteDataSource()
@@ -26,6 +15,7 @@ class DefaultTrackRepository(
     override suspend fun refreshTracks() {
         updateTracksFromRemoteDataSource()
     }
+
 
     /**
      * Forces refresh from remote data source. If refresh is successful and contains data, syncs data into database.
@@ -42,17 +32,47 @@ class DefaultTrackRepository(
     }
 
 
-    override suspend fun getAllTracks(): List<Track> {
-        val update = true
-        if (update) {
-            updateTracksFromRemoteDataSource()
-        }
-        return trackLocalDataSource.getAllTracks()
+    /**
+     *
+     */
+    override suspend fun syncTracks(tracks: List<Track>) {
+        trackLocalDataSource.syncTracks(tracks)
     }
 
 
-    override fun observeAllTracks(): LiveData<List<Track>> {
-       return trackLocalDataSource.observeAllTracks()
+    /**
+     *
+     */
+    override suspend fun favoriteTrack(timestamp: Long) {
+        trackLocalDataSource.favorite(timestamp)
+    }
+
+    override suspend fun unFavoriteTrack(timestamp: Long) {
+        trackLocalDataSource.unFavorite(timestamp)
+    }
+
+
+    /**
+     *
+     */
+    override fun observeFavorites(): LiveData<List<Track>> {
+        return trackLocalDataSource.observeFavorites()
+    }
+
+    override suspend fun getFavorites(): List<Track> {
+        return trackLocalDataSource.getFavorites()
+    }
+
+
+    /**
+     *
+     */
+    override fun observeRecent(): LiveData<List<Track>> {
+        return trackLocalDataSource.observeRecent()
+    }
+
+    override suspend fun getRecent(): List<Track> {
+        TODO("Not yet implemented")
     }
 
 
@@ -63,7 +83,6 @@ class DefaultTrackRepository(
         return trackLocalDataSource.observeGenre(genre)
     }
 
-
     /**
      * Returns List<Track> from LocalDataSource
      */
@@ -72,51 +91,73 @@ class DefaultTrackRepository(
     }
 
 
-    /** Favorites **/
-    override suspend fun favoriteTrack(timestamp: Long) {
-        trackLocalDataSource.favorite(timestamp)
-    }
-
-    override suspend fun unFavoriteTrack(timestamp: Long) {
-        trackLocalDataSource.unFavorite(timestamp)
-    }
-
-    override suspend fun getFavorites(): List<Track> {
-        return trackLocalDataSource.getFavorites()
-    }
-
-    override fun observeFavorites(): LiveData<List<Track>> {
-        return trackLocalDataSource.observeFavorites()
-    }
-
-    /** Recent Tracks **/
-    override suspend fun getRecent(): List<Track> {
-        TODO("Not yet implemented")
-    }
-    override fun observeRecent(): LiveData<List<Track>> {
-        return trackLocalDataSource.observeRecent()
-    }
-
-    /** Single Tracks **/
+    /**
+     *
+     */
     override suspend fun addTrack(track: Track) {
         track.apply {
             trackLocalDataSource.addTrack(this)
         }
     }
 
-    override suspend fun getTrack(timestamp: Long): Track {
-        return trackLocalDataSource.getTrack(timestamp)
-    }
-
-    override suspend fun updateTrack(track: Track) {
-        trackLocalDataSource.updateTrack(track)
-    }
-
     override suspend fun addAllTracks(tracks: List<Track>) {
         trackLocalDataSource.addAllTracks(tracks)
     }
 
-    override suspend fun syncTracks(tracks: List<Track>) {
-        trackLocalDataSource.syncTracks(tracks)
+
+    /**
+     *
+     */
+    override suspend fun getTrack(timestamp: Long): Track {
+        return trackLocalDataSource.getTrack(timestamp)
     }
+
+    override suspend fun getAllTracks(): List<Track> {
+        val update = true
+        if (update) {
+            updateTracksFromRemoteDataSource()
+        }
+        return trackLocalDataSource.getAllTracks()
+    }
+
+
+    /**
+     *
+     */
+    override fun observeTrack(): LiveData<Track> {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeAllTracks(): LiveData<List<Track>> {
+        return trackLocalDataSource.observeAllTracks()
+    }
+
+
+    /**
+     *
+     */
+    override suspend fun updateTrack(track: Track) {
+        trackLocalDataSource.updateTrack(track)
+    }
+
+    override suspend fun updateAllTracks(tracks: List<Track>) {
+        TODO("Not yet implemented")
+    }
+
+
+    /**
+     *
+     */
+    override suspend fun deleteTrack(track: Track) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteAllSelected(tracks: List<Track>) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteAllComplete() {
+        TODO("Not yet implemented")
+    }
+
 }
